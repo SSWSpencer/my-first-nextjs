71bf51ba2d3c55c35de22db4114f67335b6f5904
Steven Spencer <steve35spencer@gmail.com>
2020-04-23 12:49:21 -0400

    Change title

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        WOOOOOOOOOOOOOO WEBSITE
       </h1>
 
       <p className="description">
